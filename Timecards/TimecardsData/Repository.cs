6043ae9cb42134e17a325bd3c6a2ec68a4829b82
dan <danhickok@gmail.com>
2018-09-17 21:45:02 -0700

    WIP 201809172144

--- Timecards/TimecardsData/Repository.cs
+++ Timecards/TimecardsData/Repository.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using core = TimecardsCore.Models;
 
 namespace TimecardsData
 {
-    public class Repository
+    public class Repository : IDisposable
     {
+        private TimecardsContext _context = null;
+
+        public Repository() : this("TimecardsDb")
+        {
+        }
+
+        public Repository(string connectionStringName)
+        {
+            _context = new TimecardsContext(connectionStringName);
+        }
+
+        public core.Timecard GetTimecard(int ID)
+        {
+            var timecard = _context.Timecards
+                .Where(t => t.ID == ID)
+                .Select(t => t.ToCore())
+                .FirstOrDefault();
+
+            return timecard;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
     }
 }
