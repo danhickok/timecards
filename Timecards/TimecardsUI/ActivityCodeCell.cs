3d037b2a672d99f8096e32a352ee7cf7e4725a1c
Dan Hickok <danhickok@gmail.com>
2018-10-21 08:23:40 -0700

    WIP 201810210823

---
+++ Timecards/TimecardsUI/ActivityCodeCell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Following this:
+// https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/how-to-host-controls-in-windows-forms-datagridview-cells
+
+namespace TimecardsUI
+{
+    public class ActivityCodeCell : DataGridViewCell
+    {
+    }
+}
