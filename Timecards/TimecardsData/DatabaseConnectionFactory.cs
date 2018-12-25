db71041eb14572df46c657b89efbdf260e9c3401
Dan Hickok <danhickok@gmail.com>
2018-12-25 11:46:11 -0800

    wip 201812251146

---
+++ Timecards/TimecardsData/DatabaseConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimecardsData
+{
+    public class DatabaseConnectionFactory
+    {
+        public static DbConnection Create()
+        {
+
+        }
+    }
+}
