c93ff42416f1171463ad3e80db6073d1f9748eaf
Dan Hickok <danhickok@gmail.com>
2019-06-22 16:55:44 -0700

    WIP 201906221655

---
+++ Timecards/TimecardsIOC/IOCFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimecardsIOC
+{
+    public class IOCFactory : TimecardsCore.Interfaces.IIOCFactory
+    {
+        // class member
+        private static IOCFactory _self = null;
+
+        // instance member
+        private List<object> _registry;
+
+        // private constructor
+        private IOCFactory()
+        {
+            _registry = new List<object>();
+        }
+
+        // singleton
+        public static IOCFactory Self()
+        {
+            if (_self == null)
+                _self = new IOCFactory();
+            return _self;
+        }
+    }
+}
