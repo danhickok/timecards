b9da02c287767869ea5ed0e0b06e28a5c610e445
Dan Hickok <danhickok@gmail.com>
2019-11-03 07:58:02 -0800

    wip 201911030757a

---
+++ Timecards/TimecardsCore/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace TimecardsCore
+{
+    public static class Constants
+    {
+        public static (string Format, string Description)[] TimeFormatChoices = new[]
+        {
+            ("90:00<Lm", "12-hour (##:##am)"),
+            ("00:00",    "24-hour (##:##)"),
+            ("90.00<Lm", "12-hour with decimal (##.##am)"),
+            ("00.00",    "24-hour with decimal (##.##)"),
+        };
+    }
+}
