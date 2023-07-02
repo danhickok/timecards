a8f1b212b1e95128df941246462286e9a290bbbc
danhickok <danhickok@gmail.com>
2023-07-02 07:55:32 -0700

    Added project references and startup code

--- Timecards/Timecards/Program.cs
+++ Timecards/Timecards/Program.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Windows.Forms;
+using ic = TimecardsIOC;
+using tc = TimecardsCore;
+using td = TimecardsData;
+using tl = TimecardsLogger;
+using ui = TimecardsUI;
+
 namespace Timecards
 {
     internal static class Program
@@ -12,6 +20,62 @@ namespace Timecards
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // from the .NET 4.8 framework version:
+
+            // load the application configuration from the settings file
+            tc.Configuration.Load();
+
+            // create the inversion-of-control container and give it the production constants type
+            // (this establishes the path to the database for normal operation, among other things)
+            var factory = new ic.Factory();
+            factory.Register<tc.Interfaces.IAppConstants>(typeof(ProductionAppConstants), true);
+
+            // give it other types needed for the application
+            factory.Register<tc.Interfaces.IRepository>(typeof(td.Repository), false,
+                typeof(tc.Interfaces.IAppConstants));
+            factory.Register<tc.Interfaces.ILogger>(typeof(tl.Logger), true,
+                typeof(tc.Interfaces.IAppConstants));
+
+            // create the main form object and establish its last known position, if available
+            var mainForm = new ui.MainForm(factory);
+
+            if (ui.MainFormSettings.HaveBeenSet)
+            {
+                mainForm.InitialPositioning = true;
+
+                var displayArea = Screen.GetWorkingArea(new System.Drawing.Point(0, 0));
+
+                mainForm.Top = Math.Min(displayArea.Top + displayArea.Height - ui.MainFormSettings.Height,
+                    Math.Max(0, ui.MainFormSettings.Top));
+                mainForm.Left = Math.Min(displayArea.Left + displayArea.Width - ui.MainFormSettings.Width,
+                    Math.Max(0, ui.MainFormSettings.Left));
+                mainForm.Height = ui.MainFormSettings.Height;
+                mainForm.Width = ui.MainFormSettings.Width;
+
+                mainForm.InitialPositioning = false;
+            }
+            else
+            {
+                mainForm.StartPosition = FormStartPosition.WindowsDefaultLocation;
+            }
+
+            // open the main form and run the application
+            try
+            {
+                Application.Run(mainForm);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Exception: {e.Message}\n\n{e.StackTrace}", "Timecards: Untrapped Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (factory != null)
+                    factory.Dispose();
+            }
         }
     }
 }
